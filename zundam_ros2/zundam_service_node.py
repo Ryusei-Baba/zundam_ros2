53839670becacd8fbc8b0aedf547d3c01a538fb8
Ryusei Baba <s20c1102kg@s.chibakoudai.jp>
2023-06-13 21:36:59 +0900

    update zumdam_service_node.py

--- zundam_ros2/zundam_service_node.py
+++ zundam_ros2/zundam_service_node.py
@@ -1,10 +1,12 @@
 import rclpy    # ROS 2 Python モジュールのインポート
 from rclpy.node import Node # rclpy.node モジュールから Node クラスをインポート
+from zundam_ros2.srv import StringCommand
 
 class ZundamNode(Node): # ZundamNode クラス
     def __init__(self): # コンストラクタ
         super().__init__('zundam_node') #基底クラスコンストラクタの呼び出し
-        #self.
+        # サービスの生成（サービス型，サービス名，コールバック）
+        self.service = self.create_service(StringCommand, 'command', self.callback)
 
 def main(): # main() 関数
     rclpy.init()    # 初期化
