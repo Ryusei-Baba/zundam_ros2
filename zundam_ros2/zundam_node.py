e70e39e2765ea94853b59748e6b96ff34d5229fa
Ryusei Baba <s20c1102kg@s.chibakoudai.jp>
2023-06-13 21:03:21 +0900

    add srv

--- zundam_ros2/zundam_node.py
+++ zundam_ros2/zundam_node.py
@@ -1,6 +1,17 @@
-def main():
-    print('Hi from zundam_ros2.')
+import rclpy    # ROS 2 Python モジュールのインポート
+from rclpy.node import Node # rclpy.node モジュールから Node クラスをインポート
 
+class ZundamNode(Node): # ZundamNode クラス
+    def __init__(self): # コンストラクタ
+        super().__init__('zundam_node') #基底クラスコンストラクタの呼び出し
+        #self.
 
-if __name__ == '__main__':
-    main()
+def main(): # main() 関数
+    rclpy.init()    # 初期化
+    node = ZundamNode() # ノードの生成
+    try:    # 例外処理．美しく終わるため
+        rclpy.spin(node)    # ノードの処理
+    except keyboardInterrupt:
+        print('Ctrl+C が押されました．')
+    rclpy.shutdown()    # 終了処理
+    print('プログラム終了')
