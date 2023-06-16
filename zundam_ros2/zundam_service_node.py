37c14ffb1630e6f72e86818db6adcbb4b78d1c4a
Ryusei Baba <s20c1102kg@s.chibakoudai.jp>
2023-06-16 16:01:52 +0900

    update file

--- zundam_ros2/zundam_service_node.py
+++ zundam_ros2/zundam_service_node.py
@@ -2,18 +2,18 @@ import rclpy    # ROS 2 Python モジュールのインポート
 from rclpy.node import Node # rclpy.node モジュールから Node クラスをインポート
 from zundam_ros2.srv import StringCommand
 
-class ZundamNode(Node): # ZundamNode クラス
+class ZundamService(Node): # Zundam サービスクラス
     def __init__(self): # コンストラクタ
-        super().__init__('zundam_node') #基底クラスコンストラクタの呼び出し
+        super().__init__('zundam_service') #基底クラスコンストラクタの呼び出し
         # サービスの生成（サービス型，サービス名，コールバック）
         self.service = self.create_service(StringCommand, 'command', self.callback)
+        self.food = ['apple', 'banana', 'candy']
 
-def main(): # main() 関数
-    rclpy.init()    # 初期化
-    node = ZundamNode() # ノードの生成
-    try:    # 例外処理．美しく終わるため
-        rclpy.spin(node)    # ノードの処理
-    except keyboardInterrupt:
-        print('Ctrl+C が押されました．')
-    rclpy.shutdown()    # 終了処理
-    print('プログラム終了')
+    def callback(self, request, response): # コールバック
+        time.sleep(10)
+        for item in self.food:
+            if item in request.command:
+                response.answer = 'はい，これです．'
+                return response
+        response.answer = '見つけることができませんでした．'
+        return response
