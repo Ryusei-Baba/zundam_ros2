fff25ffcba35d9dc1e20e6450d3a37101dd6b0d7
Ryusei Baba <s20c1102kg@s.chibakoudai.jp>
2023-06-27 17:03:54 +0900

    update files

--- zundam_service/zundam_service/zundam_client_node.py
+++ zundam_service/zundam_service/zundam_client_node.py
@@ -1,41 +1,41 @@
 # SPDX-FileCopyrightText: 2023 Ryusei Baba <babaryusei.kw@gmail.com>
 # SPDX-License-Identifier: BSD 3-Clause
 
-import rclpy    # ROS 2 Python モジュールのインポート
-from rclpy.node import Node # rclpy.node モジュールから Node クラスをインポート
-from zundam_interfaces.srv import StringCommand # zundam_interfaces.srv モジュールからカスタムメッセージ型 StringCommand をインポート
+import rclpy
+from rclpy.node import Node
+from zundam_interfaces.srv import StringCommand
 
 
 class ZundamClient(Node): 
-    def __init__(self): # コンストラクタ
-        super().__init__('zundam_client_node') #基底クラスコンストラクタの呼び出し
-        self.client = self.create_client(StringCommand, 'command') # クライアントの生成
+    def __init__(self):
+        super().__init__('zundam_client_node')
+        self.client = self.create_client(StringCommand, 'command')
         # サービスが利用できるまで待機
         while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('サービスは利用できません．待機中...')
-        self.request = StringCommand.Request() # リクエストのインスタンス生成
+        self.request = StringCommand.Request()
 
     def send_repuest(self, order):
-        self.request.command = order # リクエストに値の代入
-        self.future = self.client.call_async(self.request) # サービスのリクエスト
+        self.request.command = order 
+        self.future = self.client.call_async(self.request)
 
 
 def main(args=None): 
-    rclpy.init(args=args)    # 初期化
-    node = ZundamClient() # ノードの生成
+    rclpy.init(args=args)   
+    node = ZundamClient() 
     order = input('何を取ってきますか：')
     node.send_repuest(order)
 
     while rclpy.ok():
         rclpy.spin_once(node)
-        if node.future.done(): # サービスの処理が終了したら
+        if node.future.done():
             try:
-                response = node.future.result() # サービスの結果をレスポンスに代入
+                response = node.future.result()
             except Exception as e:
                 node.get_logger().info(f'サービスの呼び出しは失敗しました．{e}')
             else:
-                node.get_logger().info( # 結果の表示
+                node.get_logger().info(
                     f'\n リクエスト：{node.request.command} -> レスポンス： {response.answer}')
                 break
-    rclpy.shutdown()    # 終了処理
+    rclpy.shutdown()
     print('プログラム終了')
--- zundam_service/zundam_service/zundam_service_node.py
+++ zundam_service/zundam_service/zundam_service_node.py
@@ -2,39 +2,31 @@
 # SPDX-License-Identifier: BSD 3-Clause
 
 import time
-import rclpy    # ROS 2 Python モジュールのインポート
+import rclpy
 import os
-from rclpy.node import Node # rclpy.node モジュールから Node クラスをインポート
-from zundam_interfaces.srv import StringCommand # zundam_interfaces.srv モジュールからカスタムメッセージ型 StringCommand をインポート
+from rclpy.node import Node
+from zundam_interfaces.srv import StringCommand
 from playsound import playsound
 
-class ZundamService(Node): # Zundam サービスクラス
-    def __init__(self): # コンストラクタ
-        super().__init__('zundam_service') #基底クラスコンストラクタの呼び出し
+class ZundamService(Node):
+    def __init__(self):
+        super().__init__('zundam_service')
         # サービスの生成（サービス型，サービス名，コールバック関数）
         self.service = self.create_service(StringCommand, 'command', self.callback)
         self.food = ['apple', 'banana', 'candy']
 
-    def callback(self, request, response): # コールバック
-        #time.sleep(5)
-        # for item in self.food:
-        #     if item in request.command:
-        #         response.answer = 'はい，これです．'
-        #         return response
-        # response.answer = '見つけることができませんでした．'
-        #playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../../zundam_ros2/zundam_service/voice/001_ずんだもん（ノーマル）_僕の名前はずんだも….wav'))
-        playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_ros2/zundam_service/voice/001_ずんだもん（ノーマル）_僕の名前はずんだも….wav")
-)
+    def callback(self, request, response):
+        playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_ros2/zundam_service/voice/001_ずんだもん（ノーマル）_僕の名前はずんだも….wav"))
         return response
     
     
-def main():  # main関数
-    rclpy.init() # 初期化
-    node = ZundamService() # ノードの生成
-    try: # 例外処理．美しく終わるため
-        rclpy.spin(node) # ノードの処理．コールバックを繰り返し呼び出す
+def main():
+    rclpy.init()
+    node = ZundamService()
+    try:
+        rclpy.spin(node) 
     except KeyboardInterrupt:
         print("Ctrl+C が押されました．")
     finally:
-        rclpy.shutdown() # 終了処理
+        rclpy.shutdown()
         print('プログラム終了')
